30198b98907fa992f8bd9046a21596e4db5d2cc2
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-03-11 01:15:06 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+* main - prints a string
+* Description: prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
 int main(void){
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar\, but the outcome is a piece of art\,");
 return 0;
 }
