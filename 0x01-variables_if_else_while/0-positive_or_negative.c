a585b83044068f61f694efb6d82746ec66269714
kevin kibet <kevinkipkibet@gmail.com>
2022-03-12 18:13:54 +0300

    cisfun

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,12 +18,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-	if(n<0){
-        printf("%d is negative\n", n);
-	}else if(n>0){
-	    printf("%d is positive\n", n);
-	}else{
- printf("%d is zero\n", n);
-}
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
 	return (0);
 }
