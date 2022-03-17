04253a04d925329486d67a13aff133a2f9953a7c
kevin kibet <kevinkipkibet@gmail.com>
2022-03-17 14:31:47 +0300

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* mul - returns product
-* @c: integer c
-* return: product(a*b)
-*
+* mul - returns product of a b
+* @a: first int
+* @b: second int
+* Return: product of a & b
 */
-int mul(int a, int b){
-       return a*b;
+int mul(int a, int b)
+{
+	return (a * b);
 }
- 
