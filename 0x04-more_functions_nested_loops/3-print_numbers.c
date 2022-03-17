a1a64e82b31173bc4482fb30639c3519b4a2e2ff
kevin kibet <kevinkipkibet@gmail.com>
2022-03-17 14:37:54 +0300

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print 0 to 9
+* using _putchar()
+*
+*/
+void print_numbers(void){
+     _putchar(48);
+     _putchar(49);
+     _putchar(50);
+     _putchar(51);
+     _putchar(52);
+     _putchar(53);
+     _putchar(54);
+     _putchar(55);
+     _putchar(56);
+     _putchar(57);
+     _putchar('/n');
+
+}
