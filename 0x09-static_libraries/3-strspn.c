c35f8cc7462ed67dfc687c56fe05fba208862159
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-04-01 19:13:02 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,2 +1,7 @@
 #include <main.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be searched
+ * @accept: bytes to be accepted
+ */
 unsigned int _strspn(char *s, char *accept){};
