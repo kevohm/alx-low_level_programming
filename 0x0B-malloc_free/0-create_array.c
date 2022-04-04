3cc1207ca123f903283ab05b9d2f0bee3a5e90e0
kevohm <kevinkbet@gmail.com>
2022-04-03 23:06:57 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+# include <main.h>
+# include <stdio.h>
+/**
+ * create_array - prints buffer in hexa
+ * @c: the character to initialize array with
+ * @size: the size of the memory to print     
+ * Return pointer to the array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c){
+             if(size == ){
+	     return NULL:
+	     }
+	     char *p;
+	     p = (char *)malloc(sizeof(char)*size);
+	     *p = c;
+	     return  p;
+};
