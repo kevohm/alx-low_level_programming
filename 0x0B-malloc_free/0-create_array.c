e0d075d2503becb4d490c25c089f0306586a4eaa
kevohm <kevinkbet@gmail.com>
2022-04-04 00:21:05 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,20 +4,21 @@
 /**
  * create_array - prints buffer in hexa
  * @c: the character to initialize array with
- * @size: the size of the memory to print     
- * Return pointer to the array or NULL if it fails
+ * @size: the size of the memory to print
+ * Return: char
  */
-char *create_array(unsigned int size, char c){
-            char* p;
-	   unsigned int i= 0; 
-	
-	    if(size == 0){
-	     return NULL;
-	    }
-	     p = (char *)malloc(sizeof(char)*size);
-	     while(i < size){
-	     *(p+i) = c;
-	     ++i;
-	     }
-	     return  p;
+char *create_array(unsigned int size, char c)
+
+{	
+	char *p;
+	unsigned int i= 0;
+       	if(size == 0){
+		return NULL;
+	}
+       	p = (char *)malloc(sizeof(char)*size);
+       	while(i < size)
+	       	*(p+i) = c;	
+		++i;
+	}
+	return  p;
 }
