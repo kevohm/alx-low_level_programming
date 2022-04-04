6f72c244c3e464827d852f8381026728b623856f
kevohm <kevinkbet@gmail.com>
2022-04-04 14:28:17 -0700

    done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * count - return size of array
+ * @a: array
+ * Return: size of array
+ */
+int count(char *a)
+{
+	int len;
+
+	while (*a)
+	{
+		len++;
+		a++;
+	}
+	return (len);
+}
+/**
+ * argstostr - return string of given args
+ * @ac: number of arguements
+ * @av: array of arguements
+ * Return: pointer to char
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, sum = 0;
+	char *q;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		sum += count(av[i]);
+	}
+	q = malloc(sizeof(char) * sum);
+	p = q;
+	for (i = 0; i < ac; i++)
+	{
+		while (*(av[i]))
+		{
+			*q = *(av[i]); 
+			(av[i])++;
+			q++;
+		}
+		if (*(av[i]) == '\0')
+		{
+			*q = '\n';
+			q++;
+		}
+	}q = p;
+	return (q);
+}
