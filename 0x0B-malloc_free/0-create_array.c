9e9286f87c29578a2571a3dd0accf64faef59704
kevohm <kevinkbet@gmail.com>
2022-04-04 00:34:01 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,16 +9,18 @@
  */
 char *create_array(unsigned int size, char c)
 
-{	
+{
 	char *p;
-	unsigned int i= 0;
-       	if(size == 0){
-		return NULL;
-	}
-       	p = (char *)malloc(sizeof(char)*size);
-       	while(i < size){
-	       	*(p+i) = c;	
+	unsigned int i = 0;
+	if(size == 0)
+	{
+		return (NULL);
+	}	
+	p = (char *)malloc(sizeof(char) * size);
+	while(i < size)
+	{	
+		*(p+i) = c;
 		++i;
 	}
-	return  p;
+	return (p);
 }
