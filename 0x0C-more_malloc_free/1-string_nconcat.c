5c40a449b594c2ec2b4ac06dff923d5d33fa3f38
kevohm <kevinkbet@gmail.com>
2022-04-07 07:03:58 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = lens2;
 	}
-	current = malloc( (lens1 + n) * sizeof(char));
+	current = malloc( (lens1 + n + 1) * sizeof(char));
 	start = current;
 
 	if (!current)
