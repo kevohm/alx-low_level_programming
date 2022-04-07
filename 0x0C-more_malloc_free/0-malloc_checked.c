6ec3c8462e7b9025e9ac6a0b05a4754d620b972a
kevohm <kevinkbet@gmail.com>
2022-04-07 02:57:33 -0700

    done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * malloc_checked - return pointer to alloc space
+ * @b: size to allocate
+ * Return: pointer
+ *
+ */
 void *malloc_checked(unsigned int b)
 {
 	if (malloc(b) == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
 	return malloc(b);
 }
