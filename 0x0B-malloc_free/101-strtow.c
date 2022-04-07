c411757db1e970825e8ba516e2a1520b39b1d372
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-04-07 19:13:03 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,78 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- *
- *
+ * Return: number of words
  */
+
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+
 char **strtow(char *str)
 {
-	int len = 0, i, a;
-	char *q = str;
-	char **p;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL || *str == '\0')
-	{
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+		if (words == 0)
+			return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
-	}
-	while (*str)
-	{
-		if (*str > 32)
-		{
-		}else
-		{
-			if ((*(str + 1) > 32))
-			{
-				len++;
-			}
-		}
 
-		str++;
-	} str = q;
-	p = malloc(sizeof(char*) * len);
-	i = 0, a = 0;
-
-	while(*str)
+	for (i = 0; i <= len; i++)
 	{
-		if(*str > 32)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			a++;
-			if(i > len)
+			if (c)
 			{
-				break;
-			}
-			if ( !(*(str + 1) > 32))
-			{
-				a++;
-				p[i] = malloc(sizeof(char) * a);
-				a = 0;	
-				i++;
-				str++;
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
-		str++;
-	} str = q;
-	i = 0;
-	while(*str)
-	{
-		if (*str > 32)
-		{
-			if(i > len)
-			{
-				break;
-			}
-			*(p[i]) = *str;
-			p[i]++;
-			if (!(*(str + 1) > 32))
-			{
-				*(p[i]) = '\0';
-				i++;
-			}
-		}
-		str++;
+		else if (c++ == 0)
+			start = i;
 	}
-	return (p);
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
