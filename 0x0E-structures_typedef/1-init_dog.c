ac34199e38148ba992c1871d0637ae58f998e4c0
kevohm <kevinkbet@gmail.com>
2022-04-11 05:06:09 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * init_dog - setter
+ * @d - struct dog
+ * @name - name
+ * @age - number
+ * @owner - string
+ *
+ */
 void init_dog(dog_t *d, char *name, float age, char *owner)
 {
 	d->name = name;
