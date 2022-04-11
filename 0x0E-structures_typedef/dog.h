1af35c7eea09cc7b94073cde3e954cd6a3c1b0d3
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-04-11 14:43:05 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
 
+#include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
- *
  * struct dog - dog
  * @name: name of dog
  * @age: age of dog
  * @owner: the owner
- *
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+
 #endif
