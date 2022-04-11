7d14cdf7519adce762db7087b61a8a3391ad7fcf
kevohm <kevinkbet@gmail.com>
2022-04-11 05:09:06 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner - string
  *
  */
-void init_dog(dog_t *d, char *name, float age, char *owner)
+void init_dog(struct dog  *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,5 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-void init_dog(dog_t *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
