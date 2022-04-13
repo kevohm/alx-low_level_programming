10d1d78e0962fe29ab6313bf13d8efb4e45f6d6f
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-04-13 15:59:37 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,24 +1,37 @@
+#include "3-calc.h"
 #include <stdio.h>
-#include "function_pointers.h"
-int main(int argc, char *argv[])
-{
-	int answer = get_op_func(argv[2]);
+#include <stdlib.h>
 
-	if (argc < 4)
+/**
+ * main - Entry point
+ * @argc: the number of arguments including the program name passed
+ * @argv: a pointer to pointers to the strings submitted as arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int a, b, ans;
+	int (*fp)(int, int);
+
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	if (answer == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
-	if ( (argv[2] == '/' || argv[2] == '*') && (argv[3] == 0) )
+	if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
-	answer(argv[1], argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	fp = get_op_func(argv[2]);
+	ans = fp(a, b);
+	printf("%d\n", ans);
 	return (0);
 }
