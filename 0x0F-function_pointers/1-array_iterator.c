9024daf1df39f191cd27192e3b9c56830f421f82
kevohm <kevinkbet@gmail.com>
2022-04-13 03:51:26 -0700

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * array_iterator - print each char
+ * @array: array of char
+ * @size: sizeof array
+ * @action: pointer to function
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
