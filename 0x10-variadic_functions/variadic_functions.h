6577ae26bf338736669865697081daf0c61ba8e1
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-04-14 16:44:54 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef VARIADRIC_FUNC
 #define VARIADRIC_FUNC
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
