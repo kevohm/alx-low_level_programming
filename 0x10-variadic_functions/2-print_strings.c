8c0761c7dc19aefcc6e84ab483de2cf3f9091352
kevohm <kevinkbet@gmail.com>
2022-04-14 04:44:34 -0700

    finished 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
+/**
+ * print-strings - prints strings separated by the given separator
+ * @separator: string to separate
+ * @n: no of args
+ * Return: nothing
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list list;
 
-	va_start (list, n);
+	va_start(list, n);
 	while (i < n)
 	{
-		char* p = va_arg(list, char*);
+		char *p = va_arg(list, char*);
+
 		if (p)
 		{
 			printf("%s", p);
