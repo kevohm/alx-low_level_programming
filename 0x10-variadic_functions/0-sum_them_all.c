a77a34ce84e0d71be38968475949afa8bd362847
kevohm <kevinkbet@gmail.com>
2022-04-14 02:43:52 -0700

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	int sum = 0;
+	va_list list;
+
+	va_start (list, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (i < n)
+	{
+		sum += va_arg (list, int);
+		i++;
+	}
+	va_end (list);
+	return sum;
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef VARIADRIC_FUNC
+#define VARIADRIC_FUNC
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
