dd8141c86e8a2ae72dc1e7edb34507299d6d07ff
kevohm <kevinkbet@gmail.com>
2022-04-21 07:45:45 -0700

    finished

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = strlen(str);
 	list_t *newNode = (list_t *)malloc(sizeof(list_t));
 
+	if (newNode == NULL)
+		return (NULL);
 	newNode->str = strdup(str);
 	newNode->len = len;
 	newNode->next = *head;
