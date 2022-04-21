ef7164464c1b1a04190fac9fa05d2ca002c33197
kevohm <kevinkbet@gmail.com>
2022-04-21 07:06:38 -0700

    finished 0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_list - print out node values
+ * @h: pointer to struct list_t
+ * Return: size_t
+ *
+ * Description: return size of list and print details of each struct
+ */
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
 	if (h == NULL)
 	{
-		return 0;
+		return (0);
 	}
 	while (h != NULL)
 	{
@@ -17,9 +23,10 @@ size_t print_list(const list_t *h)
 		if (p == NULL)
 		{
 			printf("[0] (nil)\n");
-		}else
+		}
+		else
 		{
-			printf("[%d] %s",size, p);
+			printf("[%d] %s", size, p);
 			printf("\n");
 		}
 		i++;
