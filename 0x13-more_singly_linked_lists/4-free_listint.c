1e14f09bb6225fce378bc6ff6c27d6fb2aaf8ea7
kevohm <kevinkbet@gmail.com>
2022-04-25 03:21:03 -0700

    finished

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - free allocated memory to list
+ * @head: pointer to struct
+ * Return: nothing
+ *
+ * Description: free memory allocated
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	ptr = head;
+	if (ptr == NULL)
+		return;
+	while (ptr != NULL)
+	{
+		free(ptr);
+		ptr = ptr->next;
+	}
+}
