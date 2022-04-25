6b302ab66b7e91698a46fc2041793f01823da960
kevohm <kevinkbet@gmail.com>
2022-04-25 01:14:17 -0700

    finished

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_listint - prints values in singly linked node
+ * @h: pointer to listint_t(struct listint_s)
+ * Return: size of list
+ *
+ * Description: prints all the elements of a listint_t list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (i);
+	}
+	while (h !=  NULL)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
