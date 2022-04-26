49970472f3efe859fe2966f427e49f97362f097c
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-04-26 16:04:59 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,7 +10,7 @@ size_t print_listint_safe(const listint_t *head)
 	int pdiff, count, a;
 
 	if (head == NULL)
-		return (0);
+		exit(98);
 	for (count = 0; head; count++)
 	{
 		pdiff = head - head->next;
