4103dd5dcf320bf0c3bcdbbc2b5eca40c6d4b44f
kevohm <kevinkbet@gmail.com>
2022-06-10 09:06:03 -0700

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	else if(idx == (i + 1))
+	else if (idx == (i + 1))
 	{
 		return (add_dnodeint_end(h, n));
 	}
