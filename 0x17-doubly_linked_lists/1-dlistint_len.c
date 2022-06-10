d0617c9e98f889842230a755474a9d4f87262eed
kevohm <kevinkbet@gmail.com>
2022-06-10 01:00:37 -0700

    fixed

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - return length
+ * @h: pointer to head node
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const slistint_t *_head = h;
+	size_t len = 0;
+
+	if (_head == NULL)
+		return (len);
+	while (_head)
+	{
+		len++;
+		_head = _head->next;
+	}
+	return (len);
+}
