cc725b99efb7a46b316c4390ef05f70350b25480
kevohm <kevinkbet@gmail.com>
2022-06-13 08:42:35 -0700

    done

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,41 @@
+#include "func.h"
+/**
+ * add - add numbers
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two numbers
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divide
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/func.h
@@ -0,0 +1,16 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+/*include*/
+#include <stdio.h>
+
+/*functions*/
+int add(int a, int b);
+
+int sub(int a, int b);
+
+int mul(int a, int b);
+
+int div(int a, int b);
+
+#endif
