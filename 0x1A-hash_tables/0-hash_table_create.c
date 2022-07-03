7dcc58dc206a48770e3ab19f72db331b7c1c1baf
kevin kibet <kevinkipkibet@gmail.com>
2022-07-03 13:26:02 +0300

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newTable = malloc(sizeof(hash_node_t *));
 	if (!newTable)
 	{
-		free(newTable);
+		free(newTable->array);
+		free(newTABLE);
 		return (NULL);
 	}
 	newTable->size = size;
