5f7efec49e2a8dd48a29988baf682f811edc38c2
kevin kibet <kevinkipkibet@gmail.com>
2022-07-03 13:13:49 +0300

    done

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create hash table
+ * @size: size of array
+ * Return: pointer to hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* newTable;
+
+	if(!size)
+		return (NULL);
+	newTable = malloc(sizeof(hash_node_t*));
+	if(!newTable)
+		free(newTable),return (NULL);
+	return (newTable);
+}
