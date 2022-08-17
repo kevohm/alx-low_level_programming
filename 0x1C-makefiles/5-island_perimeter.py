bf466323c94f81cc2aec0899c54f993969262753
kevohm <kevinkbet@gmail.com>
2022-08-17 15:18:02 -0700

    finished

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,20 +10,31 @@ def island_perimeter(grid):
     count = 0
     width = len(grid[0])
     height = len(grid)
+    st={ 
+            "r_s":False,
+            "r_e":False,
+            "c_s":False,
+            "c_e":False
+        }
     for row in range(0,height):
         for col in range(0,width):
             if grid[row][col] == 1:
-                show = False
-                try:
-                    show = (grid[row-1][col] == 0 and grid[row+1][col] == 0 and grid[row][col-1] == 0 and grid[row][col+1] == 0)
-                    if grid[row-1][col] == 0:
+                if row == 0:
+                    st["r_s"] = True
+                if col == 0:
+                    st["c_s"] = True
+                if col == width-1:
+                    st["c_e"] = True
+                if row == height-1:
+                    st["r_e"] = True
+                show = st["r_e"] and st["r_s"] and st["c_s"] and st["c_e"]
+                if not show:
+                    if grid[row-1][col] == 0 and not st["r_s"]:
                         count += 1
-                    if grid[row+1][col] == 0:
+                    if grid[row+1][col] == 0 and not st["r_e"]:
                         count += 1
-                    if grid[row][col+1] == 0:
+                    if grid[row][col+1] == 0 and not st["c_e"]:
                         count += 1
-                    if grid[row][col-1] == 0:
+                    if grid[row][col-1] == 0 and not st["c_s"]:
                         count += 1
-                except TypeError:
-                    return 0
     return count
