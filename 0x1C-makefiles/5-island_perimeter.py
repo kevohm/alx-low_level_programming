76599afb54df54bbb6cc6befa8688650e98ff430
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-08-18 01:50:29 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,47 +1,5 @@
-#!/usr/bin/python3
-"""
-    area of island
-"""
+r_e = st["r_e"]
+ 25                 r_s = st["r_s"]
+ 26                 c_e = st["c_e"]
+ 27                 c_s = st["c_s"]
 
-def island_perimeter(grid):
-    '''
-        Find perimeter of island
-    '''
-    if (type(grid) != list) or len(grid) == 0:
-        return 0
-    count = 0
-    width = len(grid[0])
-    height = len(grid)
-    r_e = False
-    r_s = False
-    c_e = False
-    c_s = False
-
-    for row in range(0,height):
-        if type(grid[row]) is not list:
-            return 0
-        for col in range(0,width):
-            if type(grid[row][col]) != int:
-                return 0
-            if grid[row][col] == 1:
-                if row == 0:
-                    r_s = True
-                if col == 0:
-                    c_s = True
-                if col == width-1:
-                    c_e = True
-                if row == height-1:
-                    r_e = True
-                if not r_e:
-                    if grid[row+1][col] == 0:
-                        count += 1
-                if not r_s:
-                    if grid[row-1][col] == 0:
-                        count += 1
-                if not c_e:
-                    if grid[row][col+1] == 0:
-                        count += 1
-                if not c_s:
-                    if grid[row][col-1] == 0:
-                        count += 1
-    return count
