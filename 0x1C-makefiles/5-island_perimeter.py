b696cb62db8a17adb0683f7fe653e56b2935d0e9
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-08-18 01:50:58 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,45 @@
-r_e = st["r_e"]
- 25                 r_s = st["r_s"]
- 26                 c_e = st["c_e"]
- 27                 c_s = st["c_s"]
+#!/usr/bin/python3
+"""5-island-perimeter module"""
 
+
+def island_perimeter(grid):
+    """island_perimeter function"""
+    if type(grid) != list or len(grid) == 0:
+        return 0
+    per = 0
+    for i in range(len(grid)):
+        if type(grid[i]) != list:
+            return 0
+        for j in range(len(grid[i])):
+            if type(grid[i][j]) != int:
+                return 0
+            if grid[i][j] == 1:
+                per += per_check(grid, i, j)
+    return per
+
+
+def per_check(grid, i, j):
+    """per_check function"""
+    per = 0
+    try:
+        if grid[i][j - 1] == 0 or j == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    try:
+        if grid[i - 1][j] == 0 or i == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    try:
+        if grid[i + 1][j] == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    try:
+        if grid[i][j + 1] == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    finally:
+        return per
